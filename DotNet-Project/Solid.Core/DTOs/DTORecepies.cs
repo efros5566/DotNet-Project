e6663cfaa1a83a7476ddfdf82be8f210831a8f1b
efros5566 <148768594+efros5566@users.noreply.github.com>
2024-06-09 00:36:30 +0300

    first commit

---
+++ DotNet-Project/Solid.Core/DTOs/DTORecepies.cs
@@ -0,0 +1,17 @@
+﻿using project_Riki_Mittelman.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solid.Core.DTOs
+{
+    public class DTORecepies
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Rating { get; set; }
+    }
+}
---
+++ DotNet-Project/Solid.Core/Models/Recepies.cs
@@ -0,0 +1,11 @@
+﻿namespace project_Riki_Mittelman.Entities
+{
+    public class Recepies
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Rating { get; set; }
+
+    }
+}
